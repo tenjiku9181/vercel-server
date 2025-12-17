ab308872a12260d2952b622265ae31f1b8849205
Taksh Gadhiya <taksh.m.gadhiya@gmail.com>
2025-12-17 14:26:18 +0530

    set as it is

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 require('dotenv').config()
- 
+
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const projectRoutes = require('./routes/projectApi')
- 
-// express app
 const app = express()
- 
-// middleware
 app.use(express.json())
 app.use(cors())
-
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
 app.use('/api/projects', projectRoutes)
-
-// connect once
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('connected to database'))
-  .catch(err => console.log('🦺🦺', err))
-
-// 🔑 EXPORT — do not listen
-module.exports = app
+  .then(() => {
+    console.log('connected to database')
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log("🦺🦺", err)
+  })
