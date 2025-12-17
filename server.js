8ea28a54ac9483e53a4329ffe64a4c9939601905
Taksh Gadhiya <taksh.m.gadhiya@gmail.com>
2025-12-17 15:41:37 +0530

    adjust

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-// require('dotenv').config()
-
-// const express = require('express')
-// const mongoose = require('mongoose')
-// const cors = require('cors')
-// const projectRoutes = require('./routes/projectApi')
-// const app = express()
-// app.use(express.json())
-// app.use(cors())
-// app.use((req, res, next) => {
-//   console.log(req.path, req.method)
-//   next()
-// })
-// app.use('/api/projects', projectRoutes)
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log('connected to database')
-//     app.listen(process.env.PORT, () => {
-//       console.log('listening for requests on port', process.env.PORT)
-//     })
-//   })
-//   .catch((err) => {
-//     console.log("🦺🦺", err)
-//   })
 require('dotenv').config()
 
 const express = require('express')
@@ -30,25 +6,18 @@ const cors = require('cors')
 const projectRoutes = require('./routes/projectApi')
 
 const app = express()
-
 app.use(express.json())
 app.use(cors())
-
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
-
 app.use('/api/projects', projectRoutes)
-
-// ---- Database Connection (Singleton Pattern) ----
 let isConnected = false
-
 async function connectDB() {
   if (isConnected) return
   await mongoose.connect(process.env.MONGO_URI)
   isConnected = true
-  console.log('connected to database')
 }
 
 // ---- Middleware Hook ----
@@ -62,5 +31,4 @@ app.use(async (req, res, next) => {
   }
 })
 
-// ---- Export, Do NOT Listen ----
 module.exports = app
